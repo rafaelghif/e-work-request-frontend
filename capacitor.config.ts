f798517b23f624b7c9d27e3bfa82a84a6f785645
rafaelghifari0310 <business.rafaelghifari@gmail.com>
2023-02-24 13:51:27 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'e-work-request-frontend',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
